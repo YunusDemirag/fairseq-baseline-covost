0c2c4164c56ca065410c0ff8e7883466ab754daf
Yunus Demirag <yunus.demirag@student.kit.edu>
2023-01-12 13:55:24 +0100

    Fixed path

--- examples/translation/preprocess_covost.py
+++ examples/translation/preprocess_covost.py
@@ -1,7 +1,7 @@
 import unicodedata
 import re
 
-COVOST_PATH = "./examples/translation/covost"
+COVOST_PATH = "./covost"
 
 def replace_german_special_chars(string: str) -> str:
 
@@ -33,7 +33,14 @@ if not os.path.exists(f'{COVOST_PATH}/tmp'):
     os.makedirs(f'{COVOST_PATH}/tmp')
 
 for split in ['dev', 'test', 'train']:
-    with open(f'{COVOST_PATH}/covost_v2.de_en.{split}.tsv', 'r') as tsv, open(f'{COVOST_PATH}/tmp/{split}.de', 'w') as txt_de, open(f'{COVOST_PATH}/tmp/{split}.en', 'w') as txt_en :
+
+    split_renaming = {
+        'dev': 'valid',
+        'test': 'test',
+        'train': 'train'
+    }
+
+    with open(f'{COVOST_PATH}/covost_v2.de_en.{split}.tsv', 'r') as tsv, open(f'{COVOST_PATH}/tmp/{split_renaming[split]}.de', 'w') as txt_de, open(f'{COVOST_PATH}/tmp/{split_renaming[split]}.en', 'w') as txt_en :
         for line in tsv.readlines():
             _path, sentence, translation, _client_id, *_ = line.split('\t')
             sentence_no_german_chars = replace_german_special_chars(sentence)
