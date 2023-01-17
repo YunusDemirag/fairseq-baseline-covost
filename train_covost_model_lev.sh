c417a7474d867a1fe6148573f2c5523b238f03d9
Yunus Demirag <yunus.demirag@student.kit.edu>
2023-01-17 10:40:12 +0100

    Updated levenshtein transformer scripts

--- train_covost_model_lev.sh
+++ train_covost_model_lev.sh
@@ -1,7 +1,7 @@
 fairseq-train \
     data-bin/covost-joined \
-    --save-dir checkpoints/levenshtein_transformer \
-    --ddp-backend=legacy_ddp \
+    --save-dir checkpoints/covost/levenshtein_transformer \
+    --ddp-backend=no_c10d \
     --task translation_lev \
     --criterion nat_loss \
     --arch levenshtein_transformer \
@@ -9,14 +9,15 @@ fairseq-train \
     --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9,0.98)' \
     --lr 0.0005 --lr-scheduler inverse_sqrt \
-    --stop-min-lr '1e-09' --warmup-updates 10000 \
+    --min-lr '1e-09' --warmup-updates 10000 \
     --warmup-init-lr '1e-07' --label-smoothing 0.1 \
     --dropout 0.3 --weight-decay 0.01 \
     --decoder-learned-pos \
     --encoder-learned-pos \
+    --apply-bert-init \
     --log-format 'simple' --log-interval 100 \
     --fixed-validation-seed 7 \
     --max-tokens 8000 \
     --save-interval-updates 10000 \
     --max-update 300000 \
-    --no-epoch-checkpoints --batch-size 32 
+    --no-epoch-checkpoints 
