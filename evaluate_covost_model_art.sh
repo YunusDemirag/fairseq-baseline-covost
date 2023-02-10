d64f8ebab44f316b526800d656ffd8b6a26f8e05
Yunus Demirag <yunus.demirag@student.kit.edu>
2023-02-10 18:05:04 +0100

    Updated for kd

--- evaluate_covost_model_art.sh
+++ evaluate_covost_model_art.sh
@@ -8,11 +8,8 @@ grep ^T ./results/covost/transformer/generate-test.txt | cut -f2- > ./results/co
 grep ^H ./results/covost/transformer/generate-test.txt | cut -f3- > ./results/covost/transformer/hypotheses.txt
 
 python format_results_for_bleu_test.py --hypotheses ./results/covost/transformer/hypotheses.txt \
-    --references ./results/covost/transformer
-/target.txt \
-    --sources ./results/covost/transformer
-/sources.txt \
-    --output ./results/covost/transformer
-/bleu_test_outputs.txt
+    --references ./results/covost/transformer/target.txt \
+    --sources ./results/covost/transformer/sources.txt \
+    --output ./results/covost/transformer/bleu_test_outputs.txt
 
 rm ./results/covost/transformer/sources.txt ./results/covost/transformer/target.txt ./results/covost/transformer/hypotheses.txt
